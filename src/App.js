70fa19ad91ae3714b31318f178adcb96bc1377ee
kupuma-ru21 <kupumaru21@gmail.com>
2021-06-12 19:48:33 +0900

    feat: create query searchRepositories

--- src/App.js
+++ src/App.js
@@ -1,16 +1,27 @@
+import { useState } from "react";
 import { ApolloProvider, Query } from "react-apollo";
 import { client } from "./client";
-import { ME } from "./graphql";
+import { SEARCH_REPOSITORIES } from "./graphql";
+
+const VARIABLES = {
+  first: 5,
+  after: null,
+  last: null,
+  before: null,
+  query: "フロントエンドエンジニア",
+};
 
 const App = () => {
+  const [variables, setVariables] = useState(VARIABLES);
+
   return (
     <ApolloProvider client={client}>
       <div>Hello</div>
-      <Query query={ME}>
+      <Query query={SEARCH_REPOSITORIES} variables={variables}>
         {({ loading, error, data }) => {
           if (loading) return "loading...";
           if (error) return <>{error.message}</>;
-          return <>{data.viewer.login}</>;
+          return <></>;
         }}
       </Query>
     </ApolloProvider>
--- src/graphql.js
+++ src/graphql.js
@@ -7,3 +7,44 @@ export const ME = gql`
     }
   }
 `;
+
+export const SEARCH_REPOSITORIES = gql`
+  query searchRepositories(
+    $first: Int
+    $after: String
+    $last: Int
+    $before: String
+    $query: String!
+  ) {
+    search(
+      first: $first
+      after: $after
+      last: $last
+      before: $before
+      query: $query
+      type: REPOSITORY
+    ) {
+      repositoryCount
+      pageInfo {
+        endCursor
+        hasNextPage
+        startCursor
+        hasPreviousPage
+      }
+      edges {
+        cursor
+        node {
+          ... on Repository {
+            id
+            name
+            url
+            stargazers {
+              totalCount
+            }
+            viewerHasStarred
+          }
+        }
+      }
+    }
+  }
+`;
