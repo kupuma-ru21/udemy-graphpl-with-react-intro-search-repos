436c59aff44790306e1cac334ad85a5ea755d0c5
kupuma-ru21 <kupumaru21@gmail.com>
2021-06-12 19:56:32 +0900

    feat: dynamic search form

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useCallback, useState } from "react";
 import { ApolloProvider, Query } from "react-apollo";
 import { client } from "./client";
 import { SEARCH_REPOSITORIES } from "./graphql";
@@ -13,14 +13,28 @@ const VARIABLES = {
 
 const App = () => {
   const [variables, setVariables] = useState(VARIABLES);
+  const handleChange = useCallback((e) => {
+    setVariables((oldVariables) => {
+      return { ...oldVariables, query: e.target.value };
+    });
+  }, []);
 
+  const { query } = variables;
   return (
     <ApolloProvider client={client}>
       <div>Hello</div>
+      <form>
+        <input
+          value={query}
+          onChange={handleChange}
+          style={{ width: "200px" }}
+        />
+      </form>
       <Query query={SEARCH_REPOSITORIES} variables={variables}>
         {({ loading, error, data }) => {
           if (loading) return "loading...";
           if (error) return <>{error.message}</>;
+          console.log(data);
           return <></>;
         }}
       </Query>
